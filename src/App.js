b6ba9da641cb33ebfe6aeba8bcf0607eb6e382ab
meldianahandayani <148156549+meldianahandayani@users.noreply.github.com>
2024-11-25 14:00:41 +0800

    App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,46 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useEffect, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setMovies, setLoading, setError } from './slices/movieSlice';
+import { setSearchQuery } from './slices/searchSlice';
 import { searchMoviesByTitle } from './searchMovies';
 import Header from './components/Header';
 import Search from './components/Search';
 import Movie from './components/Movie';
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [searchQuery, setSearchQuery] = useState('');
-  const [error, setError] = useState(null);
+  const dispatch = useDispatch();
+  const movies = useSelector((state) => state.movies.data);
+  const loading = useSelector((state) => state.movies.loading);
+  const error = useSelector((state) => state.movies.error);
+  const searchQuery = useSelector((state) => state.searchQuery);
 
   const apiKey = process.env.REACT_APP_TMDB_API_KEY;
   const apiUrl = process.env.REACT_APP_TMDB_API_URL;
+
   const fetchMovies = useCallback(async () => {
     try {
+      dispatch(setLoading(true));
       const response = await fetch(`${apiUrl}?api_key=${apiKey}&language=en-US&page=1`);
       const data = await response.json();
-      setMovies(data.results); 
+      dispatch(setMovies(data.results));
     } catch (error) {
       console.error('Error fetching data:', error);
-      setError('Failed to fetch movies. Please try again later.'); 
+      dispatch(setError('Failed to fetch movies. Please try again later.'));
     } finally {
-      setLoading(false); 
+      dispatch(setLoading(false));
     }
-  }, [apiUrl, apiKey]);
+  }, [dispatch, apiUrl, apiKey]);
+
   useEffect(() => {
     fetchMovies();
   }, [fetchMovies]);
+
   const filteredMovies = searchMoviesByTitle(movies, searchQuery);
 
   return (
     <div style={styles.container}>
       <Header title="Netflix" />
-      <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      <Search searchQuery={searchQuery} setSearchQuery={(query) => dispatch(setSearchQuery(query))} />
       {loading ? (
         <p>Loading...</p>
       ) : error ? (
@@ -47,6 +55,7 @@ function App() {
     </div>
   );
 }
+
 const styles = {
   container: { padding: '20px', fontFamily: 'Arial, sans-serif' },
   movieList: {
